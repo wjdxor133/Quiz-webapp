3e72e4ef58f7175b352f3d233f54e3a521f74caa
wjdxor133 <wjdxor1224@gmail.com>
2022-07-30 00:56:06 +0900

    add: 다시 풀기 기능 추가 - 다시 풀기 버튼을 누르면 랜딩 페이지로 이동하도록 구현함.

--- src/pages/QuizResultPage.tsx
+++ src/pages/QuizResultPage.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 
-import { Typography } from '@mui/material'
+import { Typography, Button, Stack } from '@mui/material'
+import RestartAltIcon from '@mui/icons-material/RestartAlt'
 
-import { useRecoilValue } from 'recoil'
-import { consumedTimeState } from 'states/quiz.state'
+import { useRecoilValue, useResetRecoilState } from 'recoil'
+import { quizNumberState, consumedTimeState, allSelectedAnswerState } from 'states/quiz.state'
+
+import usePageMove from 'hooks/usePageMove'
 
 import { getSeconds, getMinutes } from 'utils/time'
+import { PATH_NAME } from 'routes'
 
 import { QuizChart } from 'components'
 
 function QuizResultPage() {
   const consumedTime = useRecoilValue(consumedTimeState)
+  const seconds = getSeconds(consumedTime)
+  const minutes = getMinutes(consumedTime)
+  const handleResetQuizNum = useResetRecoilState(quizNumberState)
+  const handleResetConsumedTime = useResetRecoilState(consumedTimeState)
+  const handleResetAllSelectedAnswer = useResetRecoilState(allSelectedAnswerState)
+  const { handlePageMove } = usePageMove()
+
+  const handleReset = () => {
+    handleResetQuizNum()
+    handleResetConsumedTime()
+    handleResetAllSelectedAnswer()
+    handlePageMove(PATH_NAME['quiz'])
+  }
 
   return (
     <>
@@ -22,11 +39,18 @@ function QuizResultPage() {
         variant='h6'
         color={(theme) => theme.palette.grey[700]}
       >{`소요 시간: ${
-        60000 > consumedTime
-          ? `${getSeconds(consumedTime)}초`
-          : `${getMinutes(consumedTime)}분 ${getSeconds(consumedTime)}초`
+        60000 > consumedTime ? `${seconds}초` : `${minutes}분 ${seconds}초`
       }`}</Typography>
       <QuizChart />
+
+      <Stack direction='column' rowGap={1} mt={1}>
+        <Button variant='contained' size='large'>
+          오답노트 보러가기
+        </Button>
+        <Button variant='text' size='large' startIcon={<RestartAltIcon />} onClick={handleReset}>
+          다시 하기
+        </Button>
+      </Stack>
     </>
   )
 }
