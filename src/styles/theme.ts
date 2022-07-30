2b17bbdd90a5ad06094949c902106b92bf7e6b44
wjdxor133 <wjdxor1224@gmail.com>
2022-07-30 20:09:22 +0900

    add: 색상 지정해주기 - primary 색상만 커스텀하여 반영함.

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#62f1bd',
+      main: '#00c896',
+      dark: '#17a27d',
+      contrastText: '#fff',
+    },
+  },
+})
