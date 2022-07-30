0bd8f5a72b920a5e8463b8a079e3bbbd54140c4d
wjdxor133 <wjdxor1224@gmail.com>
2022-07-30 09:15:17 +0900

    update: 차트 색상 변경 - 차트의 정답 수, 오답 수 바의 색상을 변경함.

--- src/components/QuizChart/QuizChart.tsx
+++ src/components/QuizChart/QuizChart.tsx
@@ -1,12 +1,14 @@
 import Chart from 'react-apexcharts'
 
 import { Box } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
 
 import { useRecoilValue } from 'recoil'
 import { allSelectedAnswerState } from 'states/quiz.state'
 
 function QuizChart() {
   const { correct, incorrect } = useRecoilValue(allSelectedAnswerState)
+  const theme = useTheme()
 
   const state = {
     options: {
@@ -21,10 +23,12 @@ function QuizChart() {
       {
         name: '정답 개수',
         data: [correct.length],
+        color: theme.palette.primary.light,
       },
       {
         name: '오답 개수',
         data: [incorrect.length],
+        color: theme.palette.error.light,
       },
     ],
   }
